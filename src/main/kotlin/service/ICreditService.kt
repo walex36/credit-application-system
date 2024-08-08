fda36769adcbfdeba1f8ce67329877c05cf7a3e6
Walex <w.alex36@outlook.com>
2024-08-08 16:15:52 -0300

    update: finished services

---
+++ src/main/kotlin/service/ICreditService.kt
@@ -0,0 +1,10 @@
+package service
+
+import dev.walex.credit.application.system.entity.Credit
+import java.util.*
+
+interface ICreditService {
+    fun save(credit: Credit): Credit
+    fun findAllByCustomer(customerId: Long): List<Credit>
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
+}
---
+++ src/main/kotlin/service/ICustomerService.kt
@@ -0,0 +1,9 @@
+package service
+
+import dev.walex.credit.application.system.entity.Customer
+
+interface ICustomerService {
+    fun save(customer: Customer): Customer
+    fun findById(id: Long): Customer
+    fun delete(id: Long)
+}
